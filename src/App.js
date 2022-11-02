d0e205bb6a65b9f6bf98c27189d672bccb89605d
bidur <bidur.palikhe9090@gmail.com>
2022-11-02 13:43:05 +0545

    new palikhe website added with react

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React  from 'react';
+import "./App.css";
+import palikhe_01 from "./assets/palikhe_01.JPG";
+import palikhe_02 from "./assets/palikhe_02.jpg";
+import palikhe_03 from "./assets/palikhe_03.JPG";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+import Slider from "./components/Slider";
+
+function App() {
+  const navbarLinks = [
+    { url: "#", title: "Home" },
+    { url: "#", title: "Bangsawali" },
+  ];
+
+  return (
+    <div className="App">
+      <Navbar navbarLinks={navbarLinks} />
+      <Hero imageSrc={palikhe_01} />
+      <Slider
+        imageSrc={palikhe_02}
+        title={"We are Palikhe."}
+        subtitle={
+          "The world's biggest strength lies in the youth."
+        }
+      />
+      <Slider
+        imageSrc={palikhe_03}
+        title={"Memories for a lifetime."}
+        subtitle={"Alone we can do so little; together we can do so much."}
+        flipped={true}
+      />
+    </div>
+  );
+}
+
+export default App;
